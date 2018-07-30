6c86da01ed35f1bfe0796e588444969af8f04720
wagner junior <wmfjunior@Valdirs-Mac-mini.local>
2018-07-30 12:55:38 -0300

    add equipe footer index quase pronta

--- app/app/static/js/main.js
+++ app/app/static/js/main.js
@@ -1,9 +1,31 @@
 
+const PLACE_HOLDER_NEWSLETTER = "digite seu e-mail"
+
 $(document).ready(function () {
 
     //Realizar execucao de funcoes com custon button
     $(".btn-monteiro").click(function (element) {
         console.log(this.getAttribute('data-btn'))
+    })
+
+    // FORM NEWSLETTER paceholder manual
+    $("#newsletterform").keypress(function () {
+
+        let value = this.innerHTML.trim()
+
+        if (value == PLACE_HOLDER_NEWSLETTER) {
+            this.innerHTML = ""
+        }
+
+    })
+    $("#newsletterform").blur(function () {
+
+        let value = this.innerHTML.trim()
+
+        if (value == "") {
+            this.innerHTML = PLACE_HOLDER_NEWSLETTER
+        }
+
     })
 
 });
